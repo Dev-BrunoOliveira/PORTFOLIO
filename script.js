37c8b8ca1a2192f89245bd77f5f24695bc985ac5
Bruno Oliveira <brunooliver2015@outlook.comm>
2025-02-22 01:01:28 -0300

    pagina inicial com digitaçao

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const nav = document.querySelector("nav");
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+const nomeElemento = document.getElementById("nome");
+const texto = "Bruno Oliveira";
+let index = 0;
+
+function escrever() {
+  if (index < texto.length) {
+    nomeElemento.innerHTML += texto.charAt(index);
+    index++;
+    setTimeout(escrever, 150);
+  }
+}
+
+window.onload = () => {
+  escrever();
+};
